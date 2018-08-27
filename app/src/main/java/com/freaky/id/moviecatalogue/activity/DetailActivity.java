50b9840862fec8c7ec2584bfa49959a21ebeafbc
Yuda Eka Putra <yuda702@gmail.com>
2018-08-27 14:38:00 +0700

    Added favorite icon

--- app/src/main/java/com/freaky/id/moviecatalogue/activity/DetailActivity.java
+++ app/src/main/java/com/freaky/id/moviecatalogue/activity/DetailActivity.java
@@ -2,8 +2,10 @@ package com.freaky.id.moviecatalogue.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -17,6 +19,8 @@ public class DetailActivity extends AppCompatActivity {
     public TextView tvTitle;
     public TextView tvOverview;
     public TextView tvDate;
+    private ImageView imgFav;
+    private boolean isFav = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,7 @@ public class DetailActivity extends AppCompatActivity {
 
         Result movie = new GsonBuilder().create().fromJson(this.getIntent().getStringExtra("movie"), Result.class);
 
+        imgFav = findViewById(R.id.img_favorite);
         ivPoster = findViewById(R.id.iv_poster);
         tvTitle = findViewById(R.id.tv_title);
         tvOverview = findViewById(R.id.tv_overview);
@@ -47,5 +52,23 @@ public class DetailActivity extends AppCompatActivity {
                 .diskCacheStrategy(DiskCacheStrategy.SOURCE)
                 .fitCenter()
                 .into(ivPoster);
+
+        imgFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isFav==false) {
+                    imgFav.setImageResource(R.drawable.ic_favorite_black_24dp);
+                    isFav=true;
+                    Toast.makeText(DetailActivity.this, "Movie Favorited!",
+                            Toast.LENGTH_LONG).show();
+                }
+                else {
+                    imgFav.setImageResource(R.drawable.ic_favorite_border_black_24dp);
+                    isFav=false;
+                    Toast.makeText(DetailActivity.this, "Movie Deleted from Favorite!",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
